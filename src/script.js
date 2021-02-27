9f71843a5c27a21ac1907fbe2b478b01fb7f68a9
Simone Fiorentini <simonefiorentini1@gmail.com>
2021-02-27 19:47:42 +0200

    added gui for matcaps and number of donuts, added camera motion, partially integrated orbitControls on mobile

--- src/script.js
+++ src/script.js
@@ -19,16 +19,31 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/textures/matcaps/matcap.png')
+const matcapTexture1 = textureLoader.load('/textures/matcaps/1.png')
+const matcapTexture2 = textureLoader.load('/textures/matcaps/2.png')
+const matcapTexture3 = textureLoader.load('/textures/matcaps/3.png')
+const matcapTexture4 = textureLoader.load('/textures/matcaps/4.png')
+const matcapTexture5 = textureLoader.load('/textures/matcaps/5.png')
+const matcapTexture6 = textureLoader.load('/textures/matcaps/6.png')
+const matcapTexture7 = textureLoader.load('/textures/matcaps/7.png')
 
 
 //Fonts
 
 let text;
-
+let donutGeometry
 const donuts = [];
 const rotationRateX = []
-const rotationRateY = []
+const rotationRateY = [];
+let matcapMaterial
+let matcapAdded = false
+const matcaps = {
+    selected:  'matcapTexture5',
+}
+let donutsAmount = {
+    number :  1000
+}
+let allObjRemoved = false
 
 const fontLoader = new THREE.FontLoader();
 fontLoader.load(
@@ -59,15 +74,16 @@ fontLoader.load(
 
         textGeometry.center()
 
-        const matcapMaterial = new THREE.MeshNormalMaterial()
+        matcapMaterial = new THREE.MeshMatcapMaterial()
+        matcapMaterial.matcap = eval(matcaps.selected)
         text = new THREE.Mesh(textGeometry, matcapMaterial)
         scene.add(text)
 
-        const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
+        donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
 
         
 
-        for (let i = 0; i <= 1000; i++) {
+        for (let i = 0; i <= donutsAmount.number; i++) {
 
             donuts[i] = new THREE.Mesh(donutGeometry, matcapMaterial)
             donuts[i].position.x = (Math.random() -0.5) * 30
@@ -76,6 +92,7 @@ fontLoader.load(
 
             donuts[i].rotation.x = Math.random() * Math.PI
             donuts[i].rotation.y = Math.random() * Math.PI
+
 
             rotationRateX[i] = Math.random()
             rotationRateY[i] = Math.random()
@@ -87,6 +104,8 @@ fontLoader.load(
         }
     }
 )
+
+
 
 /**
  * Sizes
@@ -136,6 +155,14 @@ window.addEventListener('mousemove', (event) =>
     cursor.y = - (event.clientY / sizes.height - 0.5)
 })
 
+let orbitControlsEnabled = false
+//mobile ==> orbit controls
+if (window.innerWidth<900) {
+    const orbitControls = new OrbitControls(camera, canvas)
+    orbitControls.enableDamping = true   
+    orbitControlsEnabled = true
+    console.log("widnow size less than 900px: ", window.innerWidth)
+}
 
 scene.add(camera)
 
@@ -164,17 +191,74 @@ const tick = () =>
         donuts[i].rotation.y = rotationRateY[i] * elapsedTime
     }
 
-    
 
     // Update camera
 
-    camera.position.x = cursor.x * 15
-    camera.position.y = cursor.y * 15
-    camera.position.z = Math.sin((cursor.y + cursor.x) * Math.PI * 2)
+    if (orbitControlsEnabled === true) {
+    camera.position.x = Math.sin(0 -elapsedTime) * 3
+    camera.position.y = Math.sin(0 +elapsedTime) * 2
+    camera.position.z = Math.sin(0 * Math.PI +elapsedTime/10) *10
+    //update orbitControls
+    //TO FIX
+    // orbitControls.update()
+    }else{
+        camera.position.x = Math.sin(cursor.x -elapsedTime) * 3
+        camera.position.y = Math.sin(cursor.y +elapsedTime) * 2
+        camera.position.z = Math.sin((cursor.y + cursor.x) * Math.PI +elapsedTime/10) *10    
+    }
+
     if (text!==undefined) {
         camera.lookAt(text.position)
-        text.rotation.y = elapsedTime
-        text.rotation.x = Math.cos(-elapsedTime)
+        text.rotation.y = Math.sin(elapsedTime/2)
+        text.rotation.x = Math.sin(elapsedTime /6)
+    }
+    if(matcapMaterial!==undefined && !matcapAdded){
+        gui.add(matcaps, "selected", [    
+            'matcapTexture1',
+            'matcapTexture2',
+            'matcapTexture3',
+            'matcapTexture4',
+            'matcapTexture5',
+            'matcapTexture6',
+            'matcapTexture7'
+        ]).name("texture").onChange(()=>matcapMaterial.matcap = eval(matcaps.selected))
+        
+        matcapAdded = true
+        gui.add(donutsAmount, "number",0,10000).name("number of donuts").onChange(
+            ()=> {
+
+                for (let i = 0; i <=donuts.length; i++) {
+                            allObjRemoved = false
+                            scene.remove(donuts[i])
+                            if (i === donuts.length) {
+                                allObjRemoved = true
+                            }
+                        }
+    
+                        if(allObjRemoved){
+                            allObjRemoved = false
+                            for (let k = 0; k <= donutsAmount.number; k++) {
+                        
+                                donuts[k] = new THREE.Mesh(donutGeometry, matcapMaterial)
+                                donuts[k].position.x = (Math.random() -0.5) * 30
+                                donuts[k].position.y = (Math.random() -0.5) * 30
+                                donuts[k].position.z = (Math.random() -0.5) * 30
+                    
+                                donuts[k].rotation.x = Math.random() * Math.PI
+                                donuts[k].rotation.y = Math.random() * Math.PI
+                    
+                    
+                                rotationRateX[k] = Math.random()
+                                rotationRateY[k] = Math.random()
+                    
+                                const scale = Math.random()
+                                donuts[k].scale.set(scale, scale, scale)
+                    
+                                scene.add(donuts[k])
+                            }
+                        }
+            }
+        )
     }
 
 
